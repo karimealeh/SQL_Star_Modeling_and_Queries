9929e62a297532b3f9b8b221dde53dd1f911ae6a
karimealeh <81271606+karimealeh@users.noreply.github.com>
2021-03-23 23:00:03 -0600

    cleaning app.py code

--- app.py
+++ app.py
@@ -2,28 +2,24 @@ import os
 from sqlalchemy import create_engine
 from sql_tables_creation_dim_and_facts import Dim_Category, Dim_Vendor, Dim_Item, Fact_Sales, Dim_Store
 from sql_insert_into_star_schema import Dim_Store_Insert_Data, Delete_Store_Data, Delete_Vendor_Data, Dim_Vendor_Insert_Data, Delete_Item_Data, Dim_Item_Insert_Data, Delete_FactSales_Data, Fact_Sales_Insert_Data, Delete_Category_Data, Dim_Category_Insert_Data 
-
-
-
 import pandas as pd
 
+# 1 connect to sqlite database
 dbpath = os.path.join('database','mydatabase.db')
-# connect to sqlite database
 engine = create_engine(f"sqlite:///{dbpath}")
 print(Dim_Category, Dim_Vendor, Dim_Item, Fact_Sales)
 create_tables_list = [Dim_Category, Dim_Vendor, Dim_Item, Fact_Sales, Dim_Store]
 table_names = ["Dim_Category", "Dim_Vendor", "Dim_Item", "Fact_Sales", "Dim_Store"]
 counter = 0
-# creating our star schema with python 
+
+# 2 creating our star schema with python 
 for item in create_tables_list:
     print(f"creating table {table_names[counter]}")
     create_query = engine.execute(item) 
     print(f"table {table_names[counter]} was created")
     counter = counter + 1
-
-    
-    
-# read denormalized data from csv, put it into a pandas dataframe, and send to the sqlite database
+      
+# 3 read denormalized data from csv, put it into a pandas dataframe, and send to the sqlite database
 datapath = os.path.join('inputdata','liquorsalesdenormalized.csv')
 mycsvdata = pd.read_csv(datapath) 
 print("sending our data from the pandas dataframe (csv) to our sqlitedb")
@@ -31,10 +27,7 @@ print("sending our data from the pandas dataframe (csv) to our sqlitedb")
 mycsvdata.to_sql('liquorsalesdenormalized', if_exists = 'replace', con = engine)
 print("data was correctly sent to our new table")
 
-
-
-
-# Inserting data into our Dimension and Fact Tables
+# 4 Inserting data into our Dimension and Fact Tables
 list_inserts = [Dim_Store_Insert_Data, Dim_Vendor_Insert_Data, Dim_Item_Insert_Data, Fact_Sales_Insert_Data, Dim_Category_Insert_Data]
 list_deletes = [Delete_Store_Data, Delete_Vendor_Data, Delete_Item_Data, Delete_FactSales_Data, Delete_Category_Data]
 new_counter = 0
@@ -46,4 +39,4 @@ for insert in list_inserts:
     new_counter = new_counter + 1
 
 print("Done, our data is now in the star schema table in the sqlite!")
-# Done
+
